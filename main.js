3ba58df7467aeabd1928218737371b0d4409e34e
Ruhi2011 <85934962+Ruhi2011@users.noreply.github.com>
2021-11-12 17:06:03 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+console.log=("My first console project")
+var first_name="Saundarya"
+    console.log(first_name)
+    var last_name="Singh"
+    console.log(last_name)
+    var full_name=first_name.concat(last_name);
+    console.log(full_name)
+    var a=10
+    var b=9
+    var add=a+b
+    console.log(add)
+    var sub=a-b
+    console.log(sub)
+    var mult=a*b
+    console.log(mult)
+    var div=a/b
+    console.log(div)
